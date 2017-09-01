55418f7e34c3fc2030e84b3fec242dc39ee8c226
louis hellinger <louishellinger@louiss-MBP.hsd1.nj.comcast.net>
2017-08-31 21:39:41 -0400

    full working model, just need to code for only letters now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,13 @@ var guessList2 = [];
 var computerGuess = [];
 var letterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
+function createComputerGuess(rand) {
+	computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
 
+	var rand = computerGuess;
+	console.log ("this is the Computer Guess:  " + computerGuess);
+
+};
 
 function updateUsedList(){
 	document.querySelector(".guesses").innerHTML = guessList2.join("");
@@ -17,21 +23,25 @@ function updateUsedList(){
 
 function resetCounter () {
 		guessCounter = 9;
-
 		document.querySelector(".guessCount").innerHTML = "GUESSES REMAINING: " + guessCounter;
 
 
 }
 
+
 function resetGame() {
 		guessList2 = [""];
 		resetCounter();
 		updateUsedList();
+		createComputerGuess();
 		
-};
+}
+
 
 
 function buildList(theLetter){
+
+	
 			if (guessList2.indexOf(theLetter) === -1) {
 				guessList2.push(theLetter);
 				updateUsedList();
@@ -46,102 +56,42 @@ function buildList(theLetter){
 					console.log("you have used this already");
 			}
 
-};
-
-	// document.querySelector(".winsText").innerHTML = "WINS: " + wins;
-	// document.querySelector(".lossesText").innerHTML = "LOSSES: " + losses;
-	// document.querySelector(".guessCount").innerHTML = "GUESSES REMAINING: " + guessCounter;
-
-
+}
 	
 
-
-
-var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
-console.log ("this is the Computer Guess:  " + computerGuess);
-
-			document.onkeyup = function(event) {
-				var userGuess = event.key;
-			
-						if (userGuess === computerGuess) {
-							wins = wins + 1;
-							document.querySelector(".winsText").innerHTML = "WINS: " + wins;
-							resetGame();
-						
-							
-						} else if (guessCounter === 1) {
-							losses ++
-							document.querySelector(".lossesText").innerHTML = "LOSSES: " + losses;
-							resetGame();
-
-						}
-
-
-						else { 
-
-							buildList(userGuess); 
-
-						}
-					};
-
-
-					// 		else if (guessCounter > 0){
-								
-					// 	}
-								
-								
-					// 	} else if (guessCounter === 0) {
-					// 		losses = losses + 1;
-					// 		document.querySelector(".lossesText").innerHTML = "LOSSES: " + losses;
-					// 		console.log("reset game");
-					// 		resetGame();
-
-					// 	}
-
-					
-
-						
-					// }
+resetGame(computerGuess);
 
 
 
- 
+	document.onkeyup = function(event) {
 
-
-////document.querySelector(".guesses").innerHTML = guessList2.push(userGuess);
-// if (guessList2.indexOf(userGuess) > -1) {
-
-// 				alert('already selected');
-
-
-// 			} else if (letterChoices.indexOf(userGuess) > -1) {
-
-// 							guessList2.push(userGuess);
+		var userGuess = event.key;
 		
-// 				if (userGuess === computerGuess) {
-// 									winner();
-									 
-// 						} else if (guessList2.length === 9) {
-// 							guessList2 = [];
-// 							guessCounter = 9;
-// 							losses++;
-							
-// 						} else {
 
-// 					guessCounter = guessCounter -1;
+				if (userGuess === computerGuess) {
+					wins++;
+					document.querySelector(".winsText").innerHTML = "WINS: " + wins;
+					resetGame();
+				
+					
+				} else if (guessCounter === 1) {
+					losses ++;
+					document.querySelector(".lossesText").innerHTML = "LOSSES: " + losses;
+					resetGame();
 
-// 					}
-// 					}
+				}
 
-// pushes pressed key to GUESS LIST
+
+				else { 
+
+					buildList(userGuess); 
+
+				}
+			};
+
+
+
+
+
+
  
-// sets up computer logic to 
-//var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
-
-
-
-
-
-
-
-
