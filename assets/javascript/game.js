507c371e0fe6343618acb1d4f424f2bc1e382120
louis hellinger <louishellinger@louiss-MBP.hsd1.nj.comcast.net>
2017-08-31 22:13:58 -0400

    Final PUSH

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,14 @@ var losses = 0;
 var guessCounter = 9;
 var guessList = [];
 var guessList2 = [];
-var computerGuess = [];
+var computerGuess = [" "," "];
 var letterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 function createComputerGuess(rand) {
 	computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
 
 	var rand = computerGuess;
-	console.log ("this is the Computer Guess:  " + computerGuess);
+	//console.log ("this is the Computer Guess:  " + computerGuess);
 
 };
 
@@ -24,7 +24,6 @@ function updateUsedList(){
 function resetCounter () {
 		guessCounter = 9;
 		document.querySelector(".guessCount").innerHTML = "GUESSES REMAINING: " + guessCounter;
-
 
 }
 
@@ -41,7 +40,7 @@ function resetGame() {
 
 function buildList(theLetter){
 
-	
+
 			if (guessList2.indexOf(theLetter) === -1) {
 				guessList2.push(theLetter);
 				updateUsedList();
@@ -62,36 +61,34 @@ function buildList(theLetter){
 resetGame(computerGuess);
 
 
-
 	document.onkeyup = function(event) {
 
 		var userGuess = event.key;
 		
+			
+			if (letterChoices.indexOf(userGuess)  > -1) {
+							if (userGuess === computerGuess) {
+								wins++;
+								document.querySelector(".winsText").innerHTML = "WINS: " + wins;
+								resetGame();
+							
+								
+							} else if (guessCounter === 1) {
+								losses ++;
+								document.querySelector(".lossesText").innerHTML = "LOSSES: " + losses;
+								resetGame();
 
-				if (userGuess === computerGuess) {
-					wins++;
-					document.querySelector(".winsText").innerHTML = "WINS: " + wins;
-					resetGame();
-				
-					
-				} else if (guessCounter === 1) {
-					losses ++;
-					document.querySelector(".lossesText").innerHTML = "LOSSES: " + losses;
-					resetGame();
-
-				}
-
-
-				else { 
-
-					buildList(userGuess); 
-
-				}
-			};
+							}
 
 
+							else { 
+
+								buildList(userGuess); 
+
+							}
+						};
+
+			}
 
 
-
-
- 
+			 
